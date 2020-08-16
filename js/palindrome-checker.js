fa49e43ede5c17c6acf5d96a5a541c83d535e2ab
Dago R <dagoramirezserna@gmail.com>
2020-08-16 06:32:48 -0500

    Se crea la estructura de las páginas y se avanza hasta el reto 3

--- js/palindrome-checker.js
+++ js/palindrome-checker.js
@@ -7,16 +7,16 @@ function palindrome(str) {
     return strInv === str;
   }
 
-  palindrome("my eye"); // should return true.
-  palindrome("eye"); // should return true.
-  palindrome("_eye"); // should return true.
-  palindrome("race car"); // should return true.
-  palindrome("not a palindrome"); // should return false.
-  palindrome("A man, a plan, a canal. Panama"); // should return true.
-  palindrome("never odd or even"); // should return true.
-  palindrome("nope"); // should return false.
-  palindrome("almostomla"); // should return false.
-  palindrome("My age is 0, 0 si ega ym."); // should return true.
-  palindrome("1 eye for of 1 eye."); // should return fals
-  palindrome("0_0 (: /-\ :) 0-0"); // should return true.
-  palindrome("five|\_/|four"); // should return fal
+palindrome("my eye"); // should return true.
+palindrome("eye"); // should return true.
+palindrome("_eye"); // should return true.
+palindrome("race car"); // should return true.
+palindrome("not a palindrome"); // should return false.
+palindrome("A man, a plan, a canal. Panama"); // should return true.
+palindrome("never odd or even"); // should return true.
+palindrome("nope"); // should return false.
+palindrome("almostomla"); // should return false.
+palindrome("My age is 0, 0 si ega ym."); // should return true.
+palindrome("1 eye for of 1 eye."); // should return false.
+palindrome("0_0 (: /-\ :) 0-0"); // should return true.
+palindrome("five|\_/|four"); // should return false.
